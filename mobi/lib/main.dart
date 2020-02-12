88b6cb8565f203c482c9f716487593b5385cdcc9
Marcos05555 <marcos.vinicius@oceanbrasil.com>
2020-02-12 17:50:47 -0400

    17:50-12/02/2020

--- mobi/lib/main.dart
+++ mobi/lib/main.dart
@@ -7,10 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:
-          ThemeData(brightness: Brightness.light, primaryColor: Colors.green,buttonTheme: ButtonThemeData(buttonColor: Colors.green,textTheme: ButtonTextTheme.primary)),
-      home: Dashboard()
-    );
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.green,
+            buttonTheme: ButtonThemeData(
+                buttonColor: Colors.green, textTheme: ButtonTextTheme.primary)),
+        home: Dashboard());
   }
 }
 
@@ -18,31 +20,54 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title:Text('Dashboard')),
-        body: Column(
+      appBar: AppBar(title: Text('UEA')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Image.asset('image/Vestibular_UEA_2019_1.png'),
+          ),
+          ButtonText('Login',Icons.people),
+          ButtonText('Cadrastro',Icons.content_paste)
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonText extends StatelessWidget {
+  final String stringName;
+  final IconData icone;
+  ButtonText(this.stringName,this.icone);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        height: 100,
+        width: 150,
+        color: Theme.of(context).primaryColor,
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Image.asset('image/Vestibular_UEA_2019_1.png'),
+            Icon(icone,
+              color: Colors.white,
+              size: 32.0,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding:EdgeInsets.all(8.0) ,
-                height: 100,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[Icon(Icons.people,color: Colors.white,size: 32.0,), Text('LOGIN',style:TextStyle(color: Colors.white,fontSize: 16.0,)),],
-                ),
-              ),
-            )
+            Text(stringName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                )),
           ],
         ),
-      );
+      ),
+    );
   }
 }
