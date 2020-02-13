8c857cb66dc607de630c8a108e1b85b1a1d6e0a9
Macos05555 <marcos.vinicius@oceanbrasil.com>
2020-02-13 00:34:44 -0400

    00:33-13/02/2020 - Login,Cadastro e Info OK

--- mobi/lib/main.dart
+++ mobi/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobi/screens/Screen_registration.dart';
+import 'package:mobi/screens/Sreen_login.dart';
+import 'package:mobi/screens/info_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,24 +24,24 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('UEA')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-       
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Image.asset('image/Vestibular_UEA_2019_1.png'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              
-              ButtonText('LOGIN', Icons.people),
-              ButtonText('CADASTRO', Icons.content_paste),
-              ButtonText('INFORMAÇÕES', Icons.info)
-            ],
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('image/Vestibular_UEA_2019_1.png'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ButtonText('LOGIN', Icons.people, LoginScree()),
+                ButtonText('CADASTRO', Icons.content_paste, RegisterScreen()),
+                ButtonText('INFORMAÇÕES', Icons.info, InfoScreen())
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -47,34 +50,42 @@ class Dashboard extends StatelessWidget {
 class ButtonText extends StatelessWidget {
   final String stringName;
   final IconData icone;
-  ButtonText(this.stringName, this.icone);
+  final Widget screenDesired;
+  ButtonText(this.stringName, this.icone, this.screenDesired);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      padding: const EdgeInsets.all(4.0),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 4.0),
       //decoration: BoxDecoration(color: Colors.black,border: Border.all(width:1),borderRadius: BorderRadius.circular(10)),
-      child: Container(
-        padding: EdgeInsets.all(8.0),
-        height: 100,
-        width: 110,
+      child: Material(
         color: Theme.of(context).primaryColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Icon(
-              icone,
-              color: Colors.white,
-              size: 24.0,
-            ),
-            Text(stringName,
-                style: TextStyle(
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => screenDesired));
+          },
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 110,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icone,
                   color: Colors.white,
-                  fontSize: 13.0,
-                )),
-          ],
+                  size: 24.0,
+                ),
+                Text(stringName,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13.0,
+                    )),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ mobi/lib/screens/Sreen_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('LOGIN'),
+    ));
+  }
+}
