05bb6b39aec6dba095c5d031e2598006d65878f7
Macos05555 <marcos.vinicius@oceanbrasil.com>
2020-04-29 16:26:21 -0400

    29/04/2020 camera funfo

--- mobi/lib/screens/Screen_patientRegister.dart
+++ mobi/lib/screens/Screen_patientRegister.dart
@@ -1,74 +1,83 @@
 import 'dart:io';
-
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'home_screen.dart';
 import 'package:mobi/screens/Screen_registration.dart';
-File imageFile;
 
-class PatientRegisterScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var scaffold = Scaffold(
-      appBar: AppBar(
-        title: Text('Registro de Paciente'),
-      ),
-      body: SingleChildScrollView(
-              child: Column(
-          children: <Widget>[
-            LodingScreen(),
-          ],
-        ),
-      ),
-    );
-    return scaffold;
-  }
+void main() => runApp(PatientRegisterScreen());
+
+class PatientRegisterScreen extends StatefulWidget {
+  _PatientRegisterScreenState createState() => _PatientRegisterScreenState();
 }
 
-class LodingScreen extends StatefulWidget {
+class _PatientRegisterScreenState extends State<PatientRegisterScreen> {
   @override
-  _LodingScreenState createState() => _LodingScreenState();
-}
+  File _image;
 
-class _LodingScreenState extends State<LodingScreen> {
-  
-  _openCamera(BuildContext context) async{
-    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState((){
-      imageFile = picture;
+  Future getImage(bool isCamera) async {
+    File image;
+    if (isCamera) {
+      image = await ImagePicker.pickImage(source: ImageSource.camera);
+    } else {
+      image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    }
+    setState(() {
+      _image = image;
     });
-    Navigator.of(context).pop();
   }
 
-Widget _decideImageView(BuildContext context){
-  if(imageFile == null){
-    return IconButton(icon: Icon(Icons.camera_alt,color: Colors.black,size: 40,), onPressed:(){ _openCamera(context);});
+  Widget decideImageView() {
+    if (_image == null) {
+      return InkWell(
+          onTap: () {
+            getImage(true);
+          },
+          child: Image.asset(
+            'image/default-placeholder-1-2.png',
+            width: 200.0,
+            height: 200.0,
+          ));
+    } else {
+      return InkWell(
+          onTap: () {
+            getImage(true);
+          },
+          child: Image.file(
+            _image,
+            width: 300.0,
+            height: 300.0,
+          ));
+    }
   }
-  else{
-    Image.file(imageFile, width: 10,height: 10);
-  }
-}
-
-
-
-
-
-
 
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            _decideImageView(context),
-            TextBox(Icons.person, 'Nome', 'Nome do paciente'),
-              TextBox(Icons.assignment_ind, 'Idade', 'Idade do paciente'),
-          ]
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Paciente'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          //IconButton(icon:Icon(Icons.camera_alt,size: 300.0), onPressed: (){getImage(true);}),
+          SizedBox(
+            height: 10.0,
+          ),
+          decideImageView(),
+          //_image == null? Container(): Image.file(_image,height: 300.0,width: 300.0,),
+          TextBox(Icons.person, 'Nome', 'Nome do Paciente'),
+          TextBox(Icons.event_note, 'Idade', 'Idade do Paciente'),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+            },
+            child: Text(
+              'Salvar',
+              style: TextStyle(fontSize: 16),
+            ),
+          )
+        ],
+      ),
     );
   }
-
-
-
-  
 }
-
-
--- mobi/lib/screens/Screen_registration.dart
+++ mobi/lib/screens/Screen_registration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
